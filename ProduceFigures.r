c485687775da45ac23fcc92563176adfa979de93
weiyi_gisl <wc2302@columbia.edu>
2013-02-13 12:32:44 -0500

    figures finished

--- ProduceFigures.r
+++ ProduceFigures.r
@@ -1,65 +1,92 @@
-library(DreamBox7)
-library(affy)
+require(synapseClient)
+require(devtools)
+require(Biobase)
+require(survival)
+require(BCC)
 
-setwd("~/Dropbox/CU/PhD/STM/")
+install_github(repo="DreamBox7", username="weiyi-bitw", ref="master")
+library(DreamBox7)
 
 data(map)
 data(attractome.minimalist)
 
-
 #=============================================
-#	Create working space
+#	Load METABRIC and OSLO data
 #============================================
 
-load("~/workspace/data/dream7/fullrenorm/metabric2000.rdata")
+synapseLogin()
+
+metabric = loadEntity('syn1465025')$objects
+#
+# Since I don't have read access to the full metabric now, 
+# I used the following to load a local copy
+#
+#load("~/workspace/data/dream7/fullrenorm/metabric2000.rdata")
 clnc = lazyImputeDFClncOslo(metabric$Complete_METABRIC_Clinical_Features_Data)
 clinical <- expandClncOslo(clnc)
 
 ge = exprs(metabric$Complete_METABRIC_Expression_Data)
-meta = CreateMetageneSpace(ge, attractome=attractome.minimalist, map=map)
-meta = meta$metaSpace
+o = CreateMetageneSpace(ge, attractome=attractome.minimalist, map=map)
+meta = o$metaSpace
 
+# keep mesenchymal and lymph-specific metagene for conditioning
 ls = meta["ls",]
+mt = meta["mt",]
+
+# median-center each metagene
 meta = t(apply(meta, 1, function(x){ x - median(x)}))
+surv = metabric$Complete_METABRIC_Clinical_Survival_Data_DSS
 
-idx = (clinical[,"lymph_nodes_positive"]<1 & clinical[,"size"] < 30)
-mes.lymphneg = meta["mt",] * idx
-mes.lymphneg[idx] = mes.lymphneg[idx] - median(mes.lymphneg[idx])
-meta = rbind(meta, mes.lymphneg)
+# load OSLOVAL object
 
-idx = (clinical[,"lymph_nodes_positive"] > 3)
-ls.lymphpos = ls * idx
-ls.lymphpos[idx] = ls.lymphpos[idx] - median(ls.lymphpos[idx])
-meta = rbind(meta, ls.lymphpos)
+intClncDat = loadEntity("syn1449480")$objects$xIntClinDat
+intCnvEset = loadEntity("syn1449473")$objects$xCnvDat
+intSurvObject = loadEntity("syn1449477")$objects$xIntSurvObj
+intExprEset = loadEntity("syn1449475")$objects$xExprDat
 
-idx = clnc$ER.Expr == "-"
-ls.erneg = ls * idx
-ls.erneg[idx] = ls.erneg[idx] - median(ls.erneg[idx])
-meta = rbind(meta, ls.erneg)
+oslo = list(expr = intExprEset, cnv = intCnvEset, clnc = intClncDat, surv = intSurvObject)
 
-surv.os = metabric$Complete_METABRIC_Clinical_Survival_Data_OS
-surv.ds = metabric$Complete_METABRIC_Clinical_Survival_Data_DSS
+clnc.oslo = lazyImputeDFClncOslo(oslo$clnc)
+clinical.oslo <- expandClncOslo(clnc.oslo)
 
-save(clinical, clnc, meta, surv.os, surv.ds, file="~/workspace/data/dream7/fullrenorm/metabric2000.workspace.rda")
+ge.oslo = exprs(oslo$expr)
+o = CreateMetageneSpace(ge.oslo, attractome=attractome.minimalist, map=map)
+meta.oslo = o$metaSpace
 
-#===============================================
-#	directly load working space
-#===============================================
+# keep mesenchymal and lymph-specific metagene for conditioning
+ls.oslo = meta["ls",]
+mt.oslo = meta["mt",]
 
-load("~/workspace/data/dream7/fullrenorm/metabric2000.workspace.rda")
+# median-center each metagene
+meta.oslo = t(apply(meta.oslo, 1, function(x){ x - median(x)}))
+surv.oslo = oslo$surv
 
 #===============================================
 #	survival curves
 #===============================================
 
+dir.create("figs")
+
+#-------------------
+#  figure 1: mitotic
+#-------------------
+
+#
+#
+#  fig1A: metabric mitotic
+#
+#
+
 #Mitotc
 metafeature=meta["mitotic",]
-time=surv.ds[,1]
-status=surv.ds[,2]
+metafeature = metafeature - median(metafeature)
+time=surv[,1]
+status=surv[,2]
 
-idx = time >= 15*365
-time[idx] = 15*365
-status[idx] = 0
+# truncate to 15-year survival
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
 
 X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
 colnames(X)=c("time","status", "x")
@@ -67,7 +94,7 @@ fit.cin <- survfit(Surv(time, status) ~ x, data = data.frame(X))
 pval.cin<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
 
 #Plot
-tiff(file = "figs/mitotic.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+tiff(file = "figs/fig1A.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0))     #axis and label margin
 
@@ -76,7 +103,7 @@ fit.cin,
 col = c("18","20"),     #Red, Blue
 lwd = 1:1,
 mark.time = FALSE,
-main = "15-year survival based on mitotic CIN attractor expresssion level",
+main = "METABRIC",
 xlab = "Days",
 ylab = "% Survived",
 yscale = 100,
@@ -88,16 +115,38 @@ legend(2000, 0.4, c("High", "Low"),title="Mitotic CIN attractor expression level
 text(2000,0.6,paste("P-value < 2E-16 "),cex=0.5)
 dev.off()       #Write
 
-tiff(file = "figs/lsxlymNum.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+#
+#
+#  fig1B: mitotic oslo
+#
+#
+
+metafeature=meta.oslo["mitotic",]
+metafeature = metafeature - median(metafeature)
+time=surv.oslo[,1]
+status=surv.oslo[,2]
+
+# truncate to 15-year survival
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
+
+X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
+colnames(X)=c("time","status", "x")
+fit.cin.oslo <- survfit(Surv(time, status) ~ x, data = data.frame(X))
+pval.cin.oslo<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
+
+#Plot
+tiff(file = "figs/fig1B.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0))     #axis and label margin
 
 plot(
-fit.cin,
+fit.cin.oslo,
 col = c("18","20"),     #Red, Blue
 lwd = 1:1,
 mark.time = FALSE,
-main = "15-year survival based on LYM*(10-lymNum)",
+main = "OSLOVAL",
 xlab = "Days",
 ylab = "% Survived",
 yscale = 100,
@@ -105,83 +154,39 @@ cex.main = 0.5,
 cex.axis = 0.5,
 cex.lab = 0.5)
 
-legend(2000, 0.4, c("High", "Low"),title="LYM*(10-lymNum)", lwd=1:1, col=c("18","20"),cex=0.4)
-text(2000,0.6,paste("P-value < 2E-16 "),cex=0.5)
+legend(2500, 0.9, c("High", "Low"),title="Mitotic CIN attractor expression level", lwd=1:1, col=c("18","20"),cex=0.4)
+text(3000,0.3,paste("P-value =", round(pval.cin.oslo, 4)),cex=0.5)
 dev.off()       #Write
-# MES
 
-##Early Stage Samples
-idx.early = (clinical[,"lymph_nodes_positive"]<1 & clinical[,"size"] < 30)
-metafeature=meta["mes.lymphneg",idx.early]
-time=surv.ds[idx.early,1]
-status=surv.ds[idx.early,2]
+#----------------------
+#  figure 2: LYM in ER-
+#----------------------
 
-idx = time >= 10*365
-time[idx] = 10*365
-status[idx] = 0
-
-X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
-colnames(X)=c("time","status", "x")
-fit.lymphneg <- survfit(Surv(time, status) ~ x, data = data.frame(X))
-pval.lymphneg<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
-
-#Plot
-tiff(file = "figs/meslymneg30.metabric.ds.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
-par(
-mar = c(2,2,2,1),       #plot margin
-mgp = c(1, 0.4, 0)     #axis and label margin
-)         #2 columns
-
-plot(
-fit.lymphneg,
-col = c("18","20"),     #Red, Blue
-lwd = 1:1,
-mark.time = FALSE,
-main = "10-year survival based on MES metagene restricted to early-stage samples",
-xlab = "Days",
-ylab = "% Survived",
-yscale = 100,
-cex.main = 0.5,
-cex.axis = 0.5,
-cex.lab = 0.5)
-
-legend(1000, 0.4, c("High", "Low"),title="Mesenchymal transition attractor expression level", lwd=1:1, col=c("18","20"),cex=0.4)
-text(2500,0.75,paste("P-value = ",round(pval.lymphneg,4)),cex=0.5)
-dev.off()       #Write
+#
+#
+# fig2A: LYM | ER- metabric
+#
+#
 
 ##ER- Samples
 #idx.erN = (meta["er",] < 0 & meta["erbb2", ] < 0)
-idx.erN = (clnc$ER.Expr=="-" & clnc$Her2.Expr=="-" & clnc$PR.Expr=="-")
+idx.erN = clnc$ER.Expr=="-" 
 metafeature=meta["ls",idx.erN]
-time=surv.ds[idx.erN,1]
-status=surv.ds[idx.erN,2]
+metafeature = metafeature - median(metafeature)
+time=surv[idx.erN,1]
+status=surv[idx.erN,2]
 
-idx = time >= 15*365
-time[idx] = 15*365
-status[idx] = 0
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
 
 X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
 colnames(X)=c("time","status", "x")
 fit.erN <- survfit(Surv(time, status) ~ x, data = data.frame(X))
 pval.erN<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
 
-##lymphNode >3 Samples
-idx.lymph3 = (clinical[,"lymph_nodes_positive"] > 4 & clnc$ER.Expr=="+")
-metafeature=meta["ls",idx.lymph3]
-time=surv.ds[idx.lymph3,1]
-status=surv.ds[idx.lymph3,2]
-
-idx = time >= 15*365
-time[idx] = 15*365
-status[idx] = 0
-
-X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
-colnames(X)=c("time","status", "x")
-fit.lymph3 <- survfit(Surv(time, status) ~ x, data = data.frame(X))
-pval.lymph3<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
-
 #Plot
-tiff(file = "figs/lym.TN.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+tiff(file = "figs/fig2A.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(
 mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0)     #axis and label margin
@@ -193,7 +198,7 @@ fit.erN,
 col = c("18","20"),     #Red, Blue
 lwd = 1:1,
 mark.time = FALSE,
-main = "15-year survival based on LYM metagene restricted to ER-/HER2- samples",
+main = "METABRIC ER-",
 xlab = "Days",
 ylab = "% Survived",
 yscale = 100,
@@ -206,18 +211,41 @@ text(3000,0.4,paste("P-value = ",round(pval.erN,4)),cex=0.5)
 
 dev.off()
 
-tiff(file = "figs/lym.erN.lymNum4.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+#
+#
+#  fig2B: LYM | ER- oslo
+#
+#
+
+idx.erN = clnc.oslo$ER.Expr=="-" 
+metafeature=meta.oslo["ls",idx.erN]
+metafeature = metafeature - median(metafeature)
+time=surv.oslo[idx.erN,1]
+status=surv.oslo[idx.erN,2]
+
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
+
+X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
+colnames(X)=c("time","status", "x")
+fit.erN.oslo <- survfit(Surv(time, status) ~ x, data = data.frame(X))
+pval.erN.oslo<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
+
+#Plot
+tiff(file = "figs/fig2B.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(
 mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0)     #axis and label margin
-)
-#Positive lymph node number >3 Samples
+)         #2 columns
+
+#ER- Samples
 plot(
-fit.lymph3,
+fit.erN.oslo,
 col = c("18","20"),     #Red, Blue
 lwd = 1:1,
 mark.time = FALSE,
-main = "15-year survival based on LYM metagene restricted to ER+ / lymph node number > 4",
+main = "OSLOVAL ER-",
 xlab = "Days",
 ylab = "% Survived",
 yscale = 100,
@@ -226,26 +254,85 @@ cex.axis = 0.5,
 cex.lab = 0.5)
 
 legend(2000, 0.9, c("High", "Low"),title="Lymphocyte attractor expression level", lwd=1:1, col=c("18","20"),cex=0.4)
-text(3000,0.1,paste("P-value = ",round(pval.lymph3,4)),cex=0.5)
+text(3000,0.2,paste("P-value = ",round(pval.erN.oslo,4)),cex=0.5)
+
+dev.off()
+
+#-------------------
+# figure 3: LYM in ER+ with positive lymph nodes
+#-------------------
+
+# NOTE: OSLO does not have enough samples with ER+ / lymNum+ 
+# to perform a meaningful analysis
+
+##lymphNode >3 Samples
+idx.erP = (clinical[,"lymph_nodes_positive"] > 4 & clnc$ER.Expr=="+")
+metafeature=meta["ls",idx.erP]
+metafeature = metafeature - median(metafeature)
+time=surv[idx.erP,1]
+status=surv[idx.erP,2]
+
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
+
+X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
+colnames(X)=c("time","status", "x")
+fit.erP <- survfit(Surv(time, status) ~ x, data = data.frame(X))
+pval.erP<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
+
+
+tiff(file = "figs/fig3.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+par(
+mar = c(2,2,2,1),       #plot margin
+mgp = c(1, 0.4, 0)     #axis and label margin
+)
+#Positive lymph node number >3 Samples
+plot(
+fit.erP,
+col = c("18","20"),     #Red, Blue
+lwd = 1:1,
+mark.time = FALSE,
+main = "METABRIC ER+ | lymph node number > 4",
+xlab = "Days",
+ylab = "% Survived",
+yscale = 100,
+cex.main = 0.5,
+cex.axis = 0.5,
+cex.lab = 0.5)
+
+legend(2000, 0.9, c("High", "Low"),title="Lymphocyte attractor expression level", lwd=1:1, col=c("18","20"),cex=0.4)
+text(3000,0.1,paste("P-value = ",round(pval.erP,4)),cex=0.5)
 
 dev.off()       #Write
+
+#------------
+# figure 4: SUSD3-FGD3 metagene
+#------------
+
+#
+#
+# fig4A: SUSD3-FGD3 metabric
+#
+#
 
 #SUSD3-FGD3
 
 metafeature=meta["susd3",]
-time=surv.ds[,1]
-status=surv.ds[,2]
+metafeature = metafeature - median(metafeature)
+time=surv[,1]
+status=surv[,2]
 
-idx = time >= 15*365
-time[idx] = 15*365
-status[idx] = 0
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
 
 X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
 colnames(X)=c("time","status", "x")
 fit.susd3 <- survfit(Surv(time, status) ~ x, data = data.frame(X))
 pval.susd3<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
 
-tiff(file = "figs/susd3.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+tiff(file = "figs/fig4A.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(
 mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0)     #axis and label margin
@@ -256,7 +343,7 @@ fit.susd3,
 col = c("18","20"),     #Red, Blue
 lwd = 1:1,
 mark.time = FALSE,
-main = "15-year survival based on SUSD3-FGD3 metagene",
+main = "METABRIC",
 xlab = "Days",
 ylab = "% Survived",
 yscale = 100,
@@ -268,68 +355,72 @@ legend(1500, 0.4, c("High", "Low"),title="SUSD3-FGD3 metagene expression level",
 text(2000,0.6,paste("P-value < 2E-16 "),cex=0.5)
 
 dev.off()       #Write
+
+#
+#
+# fig4B: SUSD3-FGD3 OSLO
+#
+#
+
+metafeature=meta.oslo["susd3",]
+metafeature = metafeature - median(metafeature)
+time=surv.oslo[,1]
+status=surv.oslo[,2]
+
+ii = time >= 15*365
+time[ii] = 15*365
+status[ii] = 0
+
+X<-cbind(time,status,as.numeric( metafeature<median(metafeature)))
+colnames(X)=c("time","status", "x")
+fit.susd3.oslo <- survfit(Surv(time, status) ~ x, data = data.frame(X))
+pval.susd3.oslo<-summary(coxph(Surv(time, status) ~ metafeature, ))$logtest[3]
+
+tiff(file = "figs/fig4B.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+par(
+mar = c(2,2,2,1),       #plot margin
+mgp = c(1, 0.4, 0)     #axis and label margin
+)
+
+plot(
+fit.susd3.oslo,
+col = c("18","20"),     #Red, Blue
+lwd = 1:1,
+mark.time = FALSE,
+main = "OSLOVAL",
+xlab = "Days",
+ylab = "% Survived",
+yscale = 100,
+cex.main = 0.5,
+cex.axis = 0.5,
+cex.lab = 0.5)
+
+legend(2500, 0.95, c("High", "Low"),title="SUSD3-FGD3 metagene expression level", lwd=1:1, col=c("18","20"),cex=0.4)
+text(3000,0.4,paste("P-value =", round(pval.susd3.oslo, 4)),cex=0.5)
+
+dev.off()       #Write
+
 #============================
 #	Scatter plots
 #===========================
 
-load("~/workspace/data/dream7/fullrenorm/ge.49576x1981.rda")
+#------------
+# figure 5: SUSD3, FGD3, ESR1
+#------------
 
-tiff(file = "figs/ER_CIN.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+#
+#
+# SUSD3 vs FGD3
+#
+#
+
+tiff(file = "figs/fig5A.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
 par(
 mar = c(2,2,2,1),       #plot margin
 mgp = c(1, 0.4, 0)      #axis and label margin
 )
-
-#Mitotic CIN metagene vs. Estrogen Receptor Metagene
-
-plot(
-meta["mitotic",],
-ge["ILMN_1678535",], # ESR1
-lwd = 1:1,
-pch=20,         #dots
-col="blue",
-cex=0.5,
-main = "Mitotic CIN metagene expression level vs. ESR1 expression level",
-xlab = "Mitotic CIN metagene",
-ylab = "Estrogen Receptor",
-cex.main = 0.5,
-cex.axis = 0.5,
-cex.lab = 0.5)
-dev.off()       #Write
-
-#SUSD3 vs ESR1 (single gene)
-
-tiff(file = "figs/SUSD3_ER.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
-par(
-mar = c(2,2,2,1),       #plot margin
-mgp = c(1, 0.4, 0)      #axis and label margin
-)
-
-#SUSD3 vs. ESR1
-plot(
-meta["susd3",],
-meta["er",],
-lwd = 1:1,
-pch=20,         #dots
-col="blue",
-cex=0.5,
-main = "SUSD3-FGD3 metagene expression level vs. ESR1 expression level",
-xlab = "SUSD3-FGD3 metagene",
-ylab = "ER metagene ",
-cex.main = 0.5,
-cex.axis = 0.5,
-cex.lab = 0.5)
-dev.off()       #Write
 
 #SUSD3 vs. FGD3
-
-tiff(file = "figs/SUSD3_FGD3.metabric.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
-par(
-mar = c(2,2,2,1),       #plot margin
-mgp = c(1, 0.4, 0)      #axis and label margin
-)
-
-#SUSD3 vs. ESR1
 plot(
 ge["ILMN_1785570",],
 ge["ILMN_1772686",],
@@ -344,6 +435,33 @@ cex.main = 0.5,
 cex.axis = 0.5,
 cex.lab = 0.5)
 
-lines(x=c(7, 7), y=c(5, 7), col="red", lty="1")
-
 dev.off()       #Write
+
+#
+#
+# fig5B: SUSD3-FGD3 vs ESR1
+#
+#
+
+tiff(file = "figs/fig5B.tiff", width = 3.5, height = 3.5, units = "in", res = 300, compression="lzw")
+par(
+mar = c(2,2,2,1),       #plot margin
+mgp = c(1, 0.4, 0)      #axis and label margin
+)
+
+#SUSD3 vs. ESR1
+plot(
+meta["susd3",],
+ge["ILMN_1678535",],
+lwd = 1:1,
+pch=20,         #dots
+col="blue",
+cex=0.5,
+main = "SUSD3-FGD3 metagene vs. ESR1 expression level",
+xlab = "SUSD3-FGD3 metagene",
+ylab = "ESR1",
+cex.main = 0.5,
+cex.axis = 0.5,
+cex.lab = 0.5)
+dev.off()       #Write
+
